ae8085f3baf26995d71d34fa69e36c1c093f9ca5
Marco Pavan <marcopavan.mp@gmail.com>
2025-07-10 07:30:54 +0200

    refactor index

--- index.js
+++ index.js
@@ -17,20 +17,16 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the tradingradar XP project' })
 })
 
-app.use((req, res) => {
-  res.status(404).json({
-    error: 404,
-    message: 'Resource not found'
-  })
-})
-
 app.listen(port, () => {
   logger.info(`🚀 Server is running on port ${port} in ${process.env.NODE_ENV} mode.`)
 })
 
 // Connect to DB
-await connectToDB()
-cron.schedule(process.env.CRON_SCHEDULE_DB_UPDATE, async() => {
+await connectToDB() 
+if (process.env.NODE_ENV === 'development') {
   await dailyUpdateDB()
-})
-//await dailyUpdateDB()
+} else {
+  cron.schedule(process.env.CRON_SCHEDULE_DB_UPDATE, async() => {
+    await dailyUpdateDB()
+  })
+}
