0592bdb81ada71e1b85a9be3608414f9f2700424
Marco Pavan <marcopavan.mp@gmail.com>
2025-07-19 09:15:38 +0200

    completed first api test

--- app/tests/api/stock.test.js
+++ app/tests/api/stock.test.js
@@ -20,31 +20,48 @@ afterAll(async() => {
 
 beforeEach(async() => {
   await Stock.deleteMany() // pulizia prima di ogni test
+  await Stock.create([
+    {
+      '_id': '6878f2c89fc70686852ff76f',
+      'isin': 'IT0005439861',
+      '__v': 0,
+      'code': 'ABP',
+      'currency': 'EUR',
+      'market': 'Euronext Growth Milan',
+      'name': 'A.B.P. NOCIVELLI',
+      'absVariation': '-0.05',
+      'price': '5.10',
+      'relVariation': '-0.97%'
+    }
+  ])
 })
 
 describe('GET /api/stock/IT0005439861', () => {
-  it('should return a list of stocks', async() => {
-    await Stock.create([
-      {
-        '_id': '6878f2c89fc70686852ff76f',
-        'isin': 'IT0005439861',
-        '__v': 0,
-        'code': 'ABP',
-        'createdAt': '2025-07-17T12:55:36.289Z',
-        'currency': 'EUR',
-        'market': 'Euronext Growth Milan',
-        'name': 'A.B.P. NOCIVELLI',
-        'updatedAt': '2025-07-17T16:42:10.326Z',
-        'absVariation': '-0.05',
-        'price': '5.10',
-        'relVariation': '-0.97%'
-      }
-    ])
-
+  it('should return a stock object', async() => {
     const res = await request(app).get('/api/stock/IT0005439861')
-
     expect(res.statusCode).toBe(200)
-    // expect(res.body.length).toBe(1)
-    // expect(res.body[0]).toMatchObject({ symbol: 'AAPL', name: 'Apple Inc.' })
+    expect(res.body).toMatchObject({
+      '_id': '6878f2c89fc70686852ff76f',
+      'isin': 'IT0005439861',
+      '__v': 0,
+      'code': 'ABP',
+      'currency': 'EUR',
+      'market': 'Euronext Growth Milan',
+      'name': 'A.B.P. NOCIVELLI',
+      'absVariation': '-0.05',
+      'price': '5.10',
+      'relVariation': '-0.97%'
+    })
+  })
+})
+
+describe('GET /api/stock/IT0123456789', () => {
+  it('should return a 404 if wrong isin', async() => {
+    const res = await request(app).get('/api/stock/IT0123456789')
+    expect(res.statusCode).toBe(404)
+    expect(res.body).toMatchObject({
+      'error': 404,
+      'message': 'Resource not found'
+    })
   })
 })
