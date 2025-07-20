e615df46a03234faeb382cd54d124c72335ddfcc
Marco Pavan <marcopavan.mp@gmail.com>
2025-07-20 09:05:20 +0200

    CSS module error suppress + fix font path

--- app/views/home.js
+++ app/views/home.js
@@ -1,5 +1,11 @@
 export default  {
-  init: () => {
+  init: async () => {
+    const cssModule = await import('../views/home.css', {
+      with: { type: 'css' }
+    });
+    try {
+      document.adoptedStyleSheets = [cssModule.default];
+    } catch (error) {}
     // import('../views/home.css')
   }
 }
