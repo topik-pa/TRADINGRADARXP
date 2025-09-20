8b4feba4d1a85e0eaea3f8882d4f83c38cf44deb
Marco Pavan <marcopavan.mp@gmail.com>
2025-09-20 20:56:35 +0200

    bug fix on api.controller

--- app/controllers/api.controller.js
+++ app/controllers/api.controller.js
@@ -5,12 +5,10 @@ const ACCENT = 5
 export async function getStocksPerformance(req, res, next) {
   const trend = req.params.trend === 'up' ? -1 : 1
   const exchange = req.params.exchange
-  let query = { perf1M: { $exists: true, $ne: null } }
+  const query = { perf1M: { $exists: true, $ne: null } }
   if (exchange) {
     const re = new RegExp(exchange, 'i')
     query.market = re
-  } else {
-    query = {}
   }
   let stocks = []
   try {
