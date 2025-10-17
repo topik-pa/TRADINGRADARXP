fcaf98123c44cb544b138dc7897fb0749d442d3b
Marco Pavan <marcopavan.mp@gmail.com>
2025-10-17 16:28:48 +0200

    sitemap.xml dynamic

--- app/routes/ui.routes.js
+++ app/routes/ui.routes.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { Stock } from '../models/Stock.js'
 import {
   hpView,
   stockView,
@@ -8,14 +9,45 @@ import {
   exchangeView } from '../controllers/ui.controller.js'
 const router = express.Router()
 
+const BASE_URL = 'https://www.tradingradar.net'
+const LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 const CURRENT_EXCHANGES = 'milan|oslo|paris|amsterdam|brussels|lisbon|dublin|global'
 const CURRENT_LANGS = 'it|en'
 
-
 // sitemap.xml
 router.get('/sitemap.xml', (req, res) => {
-  res.sendFile('/public/sitemap.xml', { root: './app' })
+  const sitemapIndex = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${LETTERS.map(letter => `
+        <sitemap>
+          <loc>${BASE_URL}/sitemap-${letter}.xml</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+        </sitemap>
+      `).join('')}
+    </sitemapindex>`
+
+  res.header('Content-Type', 'application/xml')
+  res.send(sitemapIndex.trim())
 })
+router.get('/sitemap-:letter([A-Z]).xml', async(req, res) => {
+  const { letter } = req.params
+  const stocks = await Stock.find({ name: { $regex: `^${letter}`, $options: 'i' } })
+  const xml = `
+  <?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${stocks.map(stock => `
+      <url>
+        <loc>${BASE_URL}/${encodeURI(stock.name.toLowerCase().replaceAll(/[\s.]/g, '-').replace(/&/g, '&amp;'))}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+      </url>
+    `).join('')}
+  </urlset>`
+
+  res.header('Content-Type', 'application/xml')
+  res.send(xml.trim())
+})
+
 // robots.txt
 router.get('/robots.txt', (req, res) => {
   res.sendFile('/public/robots.txt', { root: './app' })
