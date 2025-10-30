572b53b67df1b3ff289f086ab48fb674a4190d62
Marco Pavan <marcopavan.mp@gmail.com>
2025-10-30 13:44:54 +0100

    Fix 2 bugs in dynamic update

--- app/db/setDynamicData.js
+++ app/db/setDynamicData.js
@@ -114,8 +114,9 @@ export async function setDynamicData(fod=false, partial=[]) {
         if(testEl){
           value = new JSDOM(html).window.document.querySelector('table > tbody > tr:nth-child(4) > td:nth-child(2)')?.firstChild?.nodeValue.trim() || null //!!
         }
+        value = isNaN(+value) ? null : value
       }
-      if (key==='price' || key==='volume' || key==='perf1M' || key==='perf52W') {
+      if (key==='price' || key==='volume' || key==='perf1M' || key==='perf52W' || key==='absVariation') {
         value = value?.replace(/[,]/g, '')
       }
       /*Exceptions*/
